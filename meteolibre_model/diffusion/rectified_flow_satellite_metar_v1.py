7b2eb380be2166f3ca6632b03d919d1ee60ae4cc
Adrien B <forbu14@gmail.com>
2026-07-26 01:29:34 +0200

    feat: error correction

--- meteolibre_model/diffusion/rectified_flow_satellite_metar_v1.py
+++ meteolibre_model/diffusion/rectified_flow_satellite_metar_v1.py
@@ -240,7 +240,7 @@ def apply_blur_with_sigma_batched(x, blur_sigma, n_bins=8, min_kernel=0, sigma_f
         x_bin = x[mask]
         b_bin = x_bin.shape[0]
 
-        if bin_idx == 0:
+        if s <= 0.2:
             out[mask] = x_bin
             continue
 
